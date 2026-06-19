0614e53f5f2f8c05d32f962d34cc2f700eef5331
barzlulax <dm6582100@gmail.com>
2026-06-19 13:35:58 +0700

    Update cium.js

--- plugins/cium.js
+++ plugins/cium.js
@@ -1,6 +1,18 @@
 let handler = async (m) => {
   let audioCium = 'https://files.catbox.moe/pq6khm.mp3'
   
+  // React cium dulu
+  await sock.sendMessage(m.chat, {
+    react: {
+      text: '💋',
+      key: m.key
+    }
+  })
+  
+  // Delay dikit biar rapi
+  await new Promise(resolve => setTimeout(resolve, 500))
+  
+  // Kirim audio nyium
   await sock.sendMessage(m.chat, {
     audio: { url: audioCium },
     mimetype: 'audio/mp4',
@@ -10,4 +22,5 @@ let handler = async (m) => {
 
 handler.help = ['cium']
 handler.tags = ['fun']
-handler.customPrefix = /^(cium)$/i
+handler.command = /^cium$/i
+module.exports = handler
