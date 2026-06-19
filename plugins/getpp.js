7ea5c309b716c65fa18deadacfc0de3337cef09f
barzlulax <dm6582100@gmail.com>
2026-06-19 11:18:06 +0700

    Update getpp.js

--- plugins/getpp.js
+++ plugins/getpp.js
@@ -1,10 +1,10 @@
-// plugin getpp.js - Baileys
+// plugins/getpp.js - CommonJS version
 let handler = async (m, { conn, text }) => {
   let who = m.quoted?.sender ||
             m.mentionedJid[0] ||
             (text? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.sender)
 
-  if (!who) return m.reply('Tag, reply, atau kirim nomornya dong')
+  if (!who) return m.reply('Reply chat, tag, atau ketik nomornya dong')
 
   let pp = await conn.profilePictureUrl(who, 'image')
    .catch(_ => 'https://i.imgur.com/2dzxIWP.png')
@@ -16,7 +16,8 @@ let handler = async (m, { conn, text }) => {
   }, { quoted: m })
 }
 
-handler.command = ['getpp', 'pp']
 handler.help = ['getpp @tag/nomor/reply']
 handler.tags = ['tools']
-export default handler
+handler.command = ['getpp', 'pp']
+
+module.exports = handler
