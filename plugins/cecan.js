3cee741edbb45a20369a151ca95d2aa2ef7783ee
barzlulax <dm6582100@gmail.com>
2026-06-22 20:56:50 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,53 +1,41 @@
 let fetch = require('node-fetch')
-let { generateWAMessageFromContent, proto } = require('@whiskeysockets/baileys')
+let { generateWAMessageFromContent, proto, getContentType } = require('@whiskeysockets/baileys')
 
 let handler = async (m, { sock }) => {
   let endpoints = [
     'https://api.ikyyxd.my.id/random/cecan/china',
     'https://api.ikyyxd.my.id/random/cecan/indonesia',
-    'https://api.ikyyxd.my.id/random/cecan/japan',
+    'https://api.ikyyxd.my.id/random/cecan/japan', 
     'https://api.ikyyxd.my.id/random/cecan/korea'
   ]
 
   await m.react('⏱️')
 
-  // 1. Download semua bareng pake Promise.all
-  let urls = Array.from({ length: 10 }, () => endpoints[Math.random() * endpoints.length | 0])
+  // 1. Download 10 gambar bareng
+  let urls = Array.from({length: 10}, () => endpoints[Math.random() * endpoints.length | 0])
+  let bufs = await Promise.allSettled(urls.map(u => fetch(u, {timeout: 20000}).then(r => r.buffer()).catch(() => null)))
+  let images = bufs.map(r => r.value).filter(b => b && b.length > 1000)
+  if (!images.length) return m.reply('Zonk')
 
-  let buffers = await Promise.allSettled(
-    urls.map(u => fetch(u, {timeout: 20000}).then(r => r.buffer()).catch(() => null))
-  )
+  // 2. Upload semua ke WA server dulu, kumpulin hasilnya
+  let uploaded = await Promise.all(images.map(buf => sock.waUploadToServer(buf, 'image')))
 
-  let images = buffers.map(r => r.value).filter(b => b && b.length > 1000)
-  if (!images.length) return m.reply('Zonk semua')
+  // 3. Bikin isi album: semua imageMessage langsung
+  let albumContent = uploaded.map(img => ({
+    imageMessage: img
+  }))
 
-  // 2. Bikin album message manual - INI KUNCINYA
-  let album = await generateWAMessageFromContent(m.chat, {
-    messageContextInfo: {},
-    albumMessage: {
-      expectedImageCount: images.length
-    }
-  }, { userJid: sock.user.id })
+  // 4. Bungkus jadi 1 albumMessage, kirim 1x doang
+  let msg = generateWAMessageFromContent(m.chat, {
+    albumMessage: proto.Message.AlbumMessage.fromObject({
+      expectedImageCount: images.length,
+      ...proto.Message.AlbumMessage.encode({
+        messages: albumContent
+      }).toJSON()
+    })
+  }, { quoted: m })
 
-  // 3. Upload semua gambar + masukin ke album
-  for (let i = 0; i < images.length; i++) {
-    let imgMsg = await generateWAMessageFromContent(m.chat, {
-      imageMessage: await sock.waUploadToServer(images[i], 'image')
-    }, { userJid: sock.user.id })
-
-    imgMsg.message.imageMessage.contextInfo = {
-     ...album.message.albumMessage.contextInfo,
-      remoteJid: m.chat,
-      fromMe: true,
-      stanzaId: album.key.id,
-      participant: sock.user.id
-    }
-
-    await sock.relayMessage(m.chat, imgMsg.message, { messageId: imgMsg.key.id })
-  }
-
-  // 4. Kirim album induknya paling akhir
-  await sock.relayMessage(m.chat, album.message, { messageId: album.key.id })
+  await sock.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 
   await sock.sendMessage(m.chat, { text: 'itu semua my bini barz loh 😏' }, { quoted: m })
   await m.react('✅')
